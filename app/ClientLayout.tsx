f1ad740d30330f46adbeaba684fc7f142765fdda
Sam Wertheimer <sam@sickdaysports.club>
2025-03-04 22:43:00 -0800

    feat: Add reCAPTCHA v3 integration for form protection

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -6,6 +6,7 @@ import GoogleAnalytics from '../components/GoogleAnalytics';
 import CookieConsent from '../components/CookieConsent';
 import BackToTop from '../components/BackToTop';
 import SkipToContent from '../components/SkipToContent';
+import ReCaptchaProvider from '../components/ReCaptchaProvider';
 import { initScrollDepthTracking } from '../utils/analytics';
 
 const geistSans = Geist({
@@ -36,7 +37,9 @@ export default function ClientLayout({
         {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
           <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
         )}
-        {children}
+        <ReCaptchaProvider>
+          {children}
+        </ReCaptchaProvider>
         <CookieConsent />
         <BackToTop />
       </body>
